993625d99e031e4835f7a9274355cc3f41f6c284
aFloofyDEV <129076322+aFloofyDEV@users.noreply.github.com>
2023-10-01 16:56:00 +0200

    Add files via upload

---
+++ Some silly stuffs.py
@@ -0,0 +1,18 @@
+# Some silly stuffs
+
+def func(option):
+    pi = 3.14159265358979
+    if option == 2:
+        radius = float(input("Input the circles radius: "))
+        circumference = 2 * pi * radius
+        print("The circumference is equal to:", round(circumference, 2))
+    if option == 1:
+        circumference = float(input("Input the circles circumference: "))
+        radius = circumference / (2 * pi)
+        print("The radius is equal to:", round(radius, 2))
+
+print("Welcome to Floof's universal calculator\u2122 v0.1\n\n")
+optionOut = int(input("1: Calculate radius from circumference\n2: Calculate circumference from radius\nOption: "))
+func(optionOut)
+
+input()
